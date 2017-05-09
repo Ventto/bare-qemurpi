c95989f8e4f3bf427c517bfa19f169f5dd8fa598
Thomas Venriès <thomas.venries@gmail.com>
2017-05-09 18:32:07 +0200

    test: Add newlib's printf with set r/w syscalls.

---
+++ tests/printf0/syscall.c
@@ -0,0 +1,52 @@
+#include <sys/stat.h>
+#include <stdlib.h>
+
+enum {
+    UART_FR_RXFE = 0x10,
+    UART_FR_TXFF = 0x20,
+    UART0_ADDR = 0x3f201000,
+};
+
+#define UART_DR(baseaddr) (*(unsigned int *)(baseaddr))
+#define UART_FR(baseaddr) (*(((unsigned int *)(baseaddr)) + 0x18))
+
+int _close(int file) { return -1; }
+
+int _fstat(int file, struct stat *st) { return 0; }
+
+int _isatty(int file) { return 1; }
+
+int _lseek(int file, int ptr, int dir) { return 0; }
+
+int _open(const char *name, int flags, int mode) { return -1; }
+
+int _read(int file, char *ptr, int len)
+{
+    if(len == 0)
+        return 0;
+
+    while(UART_FR(UART0_ADDR) & UART_FR_RXFE);
+
+    *ptr++ = UART_DR(UART0_ADDR);
+
+    int todo;
+    for(todo = 1; todo < len; todo++) {
+        if(UART_FR(UART0_ADDR) & UART_FR_RXFE) {
+            break;
+        }
+        *ptr++ = UART_DR(UART0_ADDR);
+    }
+    return todo;
+}
+
+caddr_t _sbrk(int incr) { return (caddr_t) 0; }
+
+int _write(int file, char *ptr, int len)
+{
+    int todo;
+
+    for (todo = 0; todo < len; todo++) {
+        UART_DR(UART0_ADDR) = *ptr++;
+    }
+    return len;
+}
