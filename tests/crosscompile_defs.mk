7e52666d48d45cf37972cbe9727505d4626b7a7a
Thomas Venriès <thomas.venries@gmail.com>
2017-05-19 18:07:00 +0200

    make: Use common mk to export toolchain and cflags vars

--- tests/crosscompile_defs.mk
+++ tests/crosscompile_defs.mk
@@ -1,7 +1,7 @@
 CROSS_COMPILE   = arm-unknown-eabi-
 AS              = $(CROSS_COMPILE)as
 LD              = $(CROSS_COMPILE)ld
-CC              = $(CROSS_COMPILE)gcc
+CC              = $(CROSS_COMPILE)gcc -mcpu=cortex-a15
 CPP             = $(CC) -E
 AR              = $(CROSS_COMPILE)ar
 NM              = $(CROSS_COMPILE)nm
@@ -10,3 +10,7 @@ OBJCOPY         = $(CROSS_COMPILE)objcopy
 OBJDUMP         = $(CROSS_COMPILE)objdump
 
 PORT_DIR		= raspi2
+
+export AR AS CC CPP LD NM OBJCOPY OBJDUMP STRIP
+
+CFLAGS += -Wall -std=c99
