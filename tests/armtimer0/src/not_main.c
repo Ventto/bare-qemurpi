778b033b7c7c6be1e81973d2d3245b8ccf362d8b
Thomas Venriès <thomas.venries@gmail.com>
2017-07-17 17:38:38 +0200

    int: Enable basic ARM irq instead of GPU.

--- tests/armtimer0/src/not_main.c
+++ tests/armtimer0/src/not_main.c
@@ -13,15 +13,12 @@ void not_main(void)
     bcm2835_armtimer_init();
 
     /* Enable ARM Timer IRQ. */
-    printf("[ BCM2835 Interrupt Controller ]\n\nEnable basic IRQs (before):"
-            "0x%08x\n",
-            (unsigned int)bcm2835_ic_get_state()->enable_basic_irqs);
+    bcm2835_ic_get_state()->enable_basic_irqs = BASIC_ARM_TIMER_IRQ;
 
-    //bcm2835_ic_get_state()->enable_basic_irqs = BASIC_ARM_TIMER_IRQ;
-    bcm2835_ic_get_state()->enable_irqs_1 = (1 << 30);
-
-    printf("Enable basic IRQs (after): 0x%08x\n",
-            (unsigned int)bcm2835_ic_get_state()->enable_basic_irqs);
+    printf("[ BCM2835 Interrupt Controller ]\n\nMemory Registers:\n"
+            "Enable_Basic_IRQs: 0x%08x\nDisable_Basic_IRQs: 0x%08x\n",
+            (unsigned int)(bcm2835_ic_get_state()->enable_basic_irqs),
+            (unsigned int)(bcm2835_ic_get_state()->disable_basic_irqs));
 
     /* Enable cores to receive any IRQs */
     enable_irqs();
